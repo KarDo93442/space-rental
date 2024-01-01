48bda996a6705672b6321e7ac1a422c9a0035fcc
Karol Do <01169552@pw.edu.pl>
2024-01-01 16:25:52 +0100

    fix typo in main

--- app/main.py
+++ app/main.py
@@ -8,12 +8,11 @@ app = Flask(__name__)
 
 @app.route('/<random_string>')
 def return_backwards_string(random_string):
-    ctn = 1
     return "".join(reversed(random_string))
 
 @app.route('/get-mode')
 def get_mode():
-    return os.environ.get_mode("MODE")
+    return os.environ.get("MODE")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
